65c4a7a8dd38b085cc3c3552e7e7e7e6f34f0b83
Willian Justen <willianjustenqui@gmail.com>
2016-06-19 02:42:42 -0300

    Add webpack example

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: './app/App.js',
+    output: {
+        path: './public', 
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3333
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
